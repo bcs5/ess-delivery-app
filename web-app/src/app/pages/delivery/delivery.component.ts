f58ef51fb4ca30cdeebbc727ff1bb5abeab33137
DiogoPedro <dpfs@cin.ufpe.br>
2022-05-02 08:48:18 -0300

    Correção de bugs da interface deliveries

--- web-app/src/app/pages/delivery/delivery.component.ts
+++ web-app/src/app/pages/delivery/delivery.component.ts
@@ -11,52 +11,54 @@ import { Delivery } from 'src/app/Interface/delivery';
 })
 export class DeliveryComponent implements OnInit {
   delivery: Delivery = new Delivery();
-   id: number;
+  id: number;
 
-   constructor(private route: ActivatedRoute, private deliveryService: DeliveriesService) {}
+  constructor(private route: ActivatedRoute, private deliveryService: DeliveriesService) { }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = +params.get('id');
       this.find();
     });
-   }
+  }
 
   find() {
     this.deliveryService.getDelivery(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => { 
+        this.delivery = delivery;
+      })
+      .catch(erro => alert(erro));
   }
-  accept () {
+  accept() {
     this.deliveryService.accept(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  reject () {
+  reject() {
     this.deliveryService.reject(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  collect () {
+  collect() {
     this.deliveryService.collect(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  finish () {
+  finish() {
     this.deliveryService.finish(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  isPending () {
+  isPending() {
     return this.delivery.status == "pending";
   }
-  isInProgress () {
+  isInProgress() {
     return this.delivery.status == "in_progress";
   }
-  isCollected () {
+  isCollected() {
     return this.delivery.status == "collected";
   }
-  isFinished () {
+  isFinished() {
     return this.delivery.status == "finished";
   }
 }
