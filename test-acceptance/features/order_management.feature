9e0e53d9a4ed8bb5a233bb4e2d6f5d31682dce01
Bezaliel Silva <bezaliel.silva@incognia.com>
2022-05-02 03:32:50 -0300

    feat(order_management.feature): functional acceptance test

---
+++ test-acceptance/features/order_management.feature
@@ -0,0 +1,29 @@
+Feature: As system manager
+         I want to mock/create some entities
+         So I can interact with them later
+  Scenario: create client
+    Given server is up
+    When I create a client with name "Bezaliel Silva", address "Rua Visconde de Barbacena, 329 - Varzea, Recife - PE", id "14"
+    Then I receive code "200"
+    And I receive a response with field "id" value "14"
+    And I receive a response with field "name" value "Bezaliel Silva"
+    And I receive a response with field "address" value "Rua Visconde de Barbacena, 329 - Varzea, Recife - PE"
+  Scenario: create restaurant
+    Given server is up
+    When I create a restaurant with name "Bob's Madalena", address "Av. Eng. Abdias de Carvalho, 365 - Ilha do Retiro, Recife - PE, 50750-257", id "13"
+    Then I receive code "200"
+    And I receive a response with field "id" value "13"
+    And I receive a response with field "name" value "Bob's Madalena"
+    And I receive a response with field "address" value "Av. Eng. Abdias de Carvalho, 365 - Ilha do Retiro, Recife - PE, 50750-257"
+  Scenario: create deliveryman
+    Given server is up
+    When I create a deliveryman with name "Jose Cruz", password "senha", id "1"
+    Then I receive code "200"
+    And I receive a response with field "id" value "1"
+    And I receive a response with field "name" value "Jose Cruz"
+  Scenario: create a order to deliveryman
+    Given server is up
+    Given I'm on the page "CinDelivery"
+    When I create a order from client "14", to restaurant "13", to deliveryman "1", payment "10", id "33"
+    Then I receive code "200"
+    And the order "33" appears on list
---
+++ test-acceptance/stepdefinitions/order_management.ts
@@ -0,0 +1,76 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+import { HttpClient, Request, sendRequest , Response} from 'selenium-webdriver/http';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let serverClient = new HttpClient("http://localhost:3000/")
+var lastResponse: Response
+
+defineSupportCode(function ({ Given, When, Then }) {
+  Given("server is up", async () => {
+    const response = await serverClient.send(new Request("GET", "/"))
+    expect(response.body).to.equal("Hello world!");
+  })
+
+  When(/^I create a client with name "([^\"]*)", address "([^\"]*)", id "(\d*)"$/, async (name, address, id) => {
+    const response = await serverClient.send(new Request("POST", "/client", {
+      "id": id,
+      "name": name,
+      "address": address
+    }))
+    lastResponse = response
+  })
+
+  Given(/^I'm on the page "([^\"]*)"$/, async (name) => {
+    await browser.get("http://localhost:4200/deliveries");
+    await expect(browser.getTitle()).to.eventually.equal(name);
+  })
+
+  When(/^I create a restaurant with name "([^\"]*)", address "([^\"]*)", id "(\d*)"$/, async (name, address, id) => {
+    const response = await serverClient.send(new Request("POST", "/restaurant", {
+      "id": id,
+      "name": name,
+      "address": address
+    }))
+    lastResponse = response
+  })
+
+  When(/^I create a deliveryman with name "([^\"]*)", password "([^\"]*)", id "(\d*)"$/, async (name, password, id) => {
+    const response = await serverClient.send(new Request("POST", "/deliveryman", {
+      "id": id,
+      "name": name,
+      "password": password
+    }))
+    lastResponse = response
+  })
+
+  When(/^I create a order from client "([^\"]*)", to restaurant "([^\"]*)", to deliveryman "([^\"]*)", payment "(\d*)", id "(\d*)"$/, async (client, restaurant, deliveryman, payment, id) => {
+    const response = await serverClient.send(new Request("POST", "/order", {
+      "id": id,
+      "clientId": client,
+      "restaurantId": restaurant,
+      "payment": payment,
+      "deliverymanId": deliveryman
+    }))
+    lastResponse = response
+  })
+
+  Then(/^I receive code "(\d*)"$/, async (statusCode) => {
+    expect(lastResponse.status.toString()).to.equal(statusCode)
+  })
+
+  Then(/^the order "(\d*)" appears on list$/, async (orderId) => {
+    await sleep(100)
+    await element(by.id("refresh")).click();
+    await sleep(500)
+    expect((await element.all(by.id(`delivery-${orderId}`))).length).to.equal(1);
+  })
+
+  Then(/^I receive a response with field "([^\"]*)" value "([^\"]*)"$/, async (field, value) => {
+    expect(JSON.parse(lastResponse.body)[field.toString()]).to.equal(value)
+  })
+})
