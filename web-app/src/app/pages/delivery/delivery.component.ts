726bd0d93684a2f64660dbc2999d35ad3b0d3202
DiogoPedro <dpfs@cin.ufpe.br>
2022-05-02 08:48:18 -0300

    Correção de bugs da interface deliveries

--- web-app/src/app/pages/delivery/delivery.component.ts
+++ web-app/src/app/pages/delivery/delivery.component.ts
@@ -11,42 +11,49 @@ import { Delivery } from 'src/app/Interface/delivery';
 })
 export class DeliveryComponent implements OnInit {
   delivery: Delivery = new Delivery();
-   id: number;
+  id: number;
 
+<<<<<<< HEAD
    constructor(private route: ActivatedRoute, private deliveryService: DeliveriesService, private router: Router) {}
+=======
+  constructor(private route: ActivatedRoute, private deliveryService: DeliveriesService) { }
+>>>>>>> f58ef51 (Correção de bugs da interface deliveries)
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = +params.get('id');
       this.find();
     });
-   }
+  }
 
   find() {
     this.deliveryService.getDelivery(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => { 
+        this.delivery = delivery;
+      })
+      .catch(erro => alert(erro));
   }
-  accept () {
+  accept() {
     this.deliveryService.accept(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  reject () {
+  reject() {
     this.deliveryService.reject(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  collect () {
+  collect() {
     this.deliveryService.collect(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
-  finish () {
+  finish() {
     this.deliveryService.finish(this.id)
-    .then(delivery => this.delivery = delivery)
-    .catch(erro => alert(erro));
+      .then(delivery => this.delivery = delivery)
+      .catch(erro => alert(erro));
   }
+<<<<<<< HEAD
   evaluation(cScore: number, rScore:number){
     this.deliveryService.evaluation(this.id,rScore,cScore)
     .then(delivery => this.delivery = delivery)
@@ -56,15 +63,18 @@ export class DeliveryComponent implements OnInit {
     this.router.navigateByUrl('/deliveries');
   }
   isPending () {
+=======
+  isPending() {
+>>>>>>> f58ef51 (Correção de bugs da interface deliveries)
     return this.delivery.status == "pending";
   }
-  isInProgress () {
+  isInProgress() {
     return this.delivery.status == "in_progress";
   }
-  isCollected () {
+  isCollected() {
     return this.delivery.status == "collected";
   }
-  isFinished () {
+  isFinished() {
     return this.delivery.status == "finished";
   }
   isEvaluated () {
