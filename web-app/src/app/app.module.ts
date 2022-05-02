da6e4a5ac0345c6cb762dfb26ba3a08589be11fb
Dara Vasconcelos <dcsv@cin.ufpe.br>
2022-05-02 08:45:45 -0300

    [cadastro]apply form validators and form submit action

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -32,6 +32,7 @@ import { RegisterComponent } from './pages/register/register.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {
